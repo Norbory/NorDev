1cba07a37c8e5a684edcdb80a41d339055dd4c18
Norbory <mandros.angelof@pucp.edu.pe>
2023-12-30 18:15:23 -0500

    Add custom colours

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors:{
+			"sunny": "#FFFFFF",
+			"main": "#EF233C",
+			"idle":"#D1D1D1",
+			"active":"#252525",
+		},
 		extend: {},
 	},
 	plugins: [],
