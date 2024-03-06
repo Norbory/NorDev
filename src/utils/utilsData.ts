ae136434349bb2965f1cd69f1d444e8d6292001b
Norbory <mandros.angelof@pucp.edu.pe>
2024-03-06 10:51:54 -0500

    Journey

--- src/utils/utilsData.ts
+++ src/utils/utilsData.ts
@@ -4,8 +4,9 @@ import { SECRET_CHAT, INSTAGRAM_CLONE, WAREHOUSE, GEOCLOUD } from '../constants/
 export const project: Project[] = [
     
         {
+            id: 0,
             title: "Instagram Clone",
-            description: "Developing an Instagram clone utilizing WebSockets for real-time notifications. Features include secure login, user profiles, social interactions, real-time feed updates and notifications. Aimed at providing practical experience with modern web technologies and real-time functionalities. Use Amandros as user to login.",
+            description: "Developing an Instagram clone utilizing WebSockets for real-time notifications. Features include secure login, user profiles, social interactions, real-time feed updates and notifications. Aimed at providing practical experience with modern web technologies and real-time functionalities.",
             linkfront: "https://notify-me-lovat.vercel.app/",
             linkback: "https://github.com/Norbory/NotifyMe_Server",
             code: "https://github.com/Norbory/NotifyMe",
@@ -13,6 +14,7 @@ export const project: Project[] = [
             tools: ["React", "Vite", "Node.js", "Express", "Socket.io", "Cloudinary", "TailwindCSS"]
         },
         {
+            id: 1,
             title: "Secret Chat",
             description: "Real-time editable notes board built using Node.js, Express, MongoDB, and sockets. It allows users to create, view, edit, and delete notes collaboratively. Node.js with Express handles the server-side logic and API endpoints, while MongoDB stores the notes data. Socket.IO facilitates real-time communication, ensuring that changes made by one user are instantly reflected across all connected clients viewing the same note.",
             linkfront: "",
@@ -22,6 +24,7 @@ export const project: Project[] = [
             tools: ["Node.js", "Express", "MongoDB", "Socket.io", "TailwindCSS"]
         },
         {
+            id: 2,
             title: "Geodigital Warehouse",
             description: "Geohidrac S.A.C.'s digital warehouse, powered by React, Supabase, Next.js, and Tailwind CSS, revolutionizes inventory management for mining operations. Combining Next.js for server-side rendering and Supabase for backend operations ensures scalability and security. Tailwind CSS facilitates responsive design. The application includes features such as real-time inventory tracking, barcode integration, low stock notifications, and comprehensive reporting. This streamlined solution optimizes Geohidrac S.A.C.'s warehouse operations in mining environments efficiently..",
             linkfront: "https://geo-digital.vercel.app/",
@@ -31,6 +34,7 @@ export const project: Project[] = [
             tools: ["React", "NextJS", "Supabase", "TailwindCSS"]
         },
         {
+            id: 3,
             title: "Geocloud",
             description: "Geohidrac S.A.C.'s project involves gathering sensor data from various mines across Peru. Leveraging tools such as React, Vite, DynamoDB, Amazon SNS, Tailwind CSS, IoT Core, and Google Maps API, the project creates a robust system. IoT technologies, including RFID, are employed to collect data on-site and upload it to the cloud seamlessly. This comprehensive approach enables Geohidrac S.A.C. to efficiently monitor and manage mining operations, enhancing safety, productivity, and decision-making processes.",
             linkfront: "",
