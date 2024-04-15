1bf5b57b08b1267ec4f7c9f6ff6cd97ccff7bc8a
Norbory <mandros.angelof@pucp.edu.pe>
2024-04-14 23:39:12 -0500

    Optimize images

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,5 +1,5 @@
-export const SECRET_CHAT = "https://res.cloudinary.com/dmbtlv0hg/image/upload/v1709053396/samples/port/notas_jsarhi.png";
-export const INSTAGRAM_CLONE = "https://res.cloudinary.com/dmbtlv0hg/image/upload/v1709052951/samples/port/clon_zoaira.png";
-export const WAREHOUSE = "https://res.cloudinary.com/dmbtlv0hg/image/upload/v1709096924/samples/port/dsds_srv2uz.png";
-export const GEOCLOUD = "https://res.cloudinary.com/dmbtlv0hg/image/upload/v1709126643/samples/port/mapa_lolwjd.png";
-export const SHOES = "https://res.cloudinary.com/dmbtlv0hg/image/upload/v1713121731/samples/port/zapatillas_cdxtgl.png";
+export const SECRET_CHAT = "/notas.webp";
+export const INSTAGRAM_CLONE = "/clon.webp";
+export const WAREHOUSE = "/dsds.webp";
+export const GEOCLOUD = "/mapa.webp";
+export const SHOES = "/zapatillas.webp";
