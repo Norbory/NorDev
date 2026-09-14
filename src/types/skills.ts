c6d381c5e99af43b5acb8646513779fc1933dc72
Angelo Mandros <mandros.angelof@pucp.edu.pe>
2026-09-13 20:39:08 -0500

    Feat: Add skills zone

---
+++ src/types/skills.ts
@@ -0,0 +1,4 @@
+export interface Skills{
+    tipo: string,
+    name: string
+}
---
+++ src/utils/skillsData.ts
@@ -0,0 +1,64 @@
+import { type Skills } from '../types/skills';
+
+export const skills: Skills[] = [
+    {
+        tipo: "FE_01",
+        name: "TYPESCRIPT/JAVASCRIPT"
+    },
+    {
+        tipo: "FE_02",
+        name: "REACT/ANGULAR/ASTRO"
+    },
+    {
+        tipo: "FE_03",
+        name: "REACT NATIVE"
+    },
+    {
+        tipo: "BE_01",
+        name: "NODE/EXPRESS/NEST"
+    },
+    {
+        tipo: "BE_02",
+        name: ".NET"
+    },
+    {
+        tipo: "BE_03",
+        name: "RUST/RUBY ON RAILS"
+    },
+    {
+        tipo: "DB_01",
+        name: "POSTGRESQL/ORACLE"
+    },
+    {
+        tipo: "DB_02",
+        name: "PLSQL/MYSQL"
+    },
+    {
+        tipo: "DB_03",
+        name: "MONGODB/DYNAMODB"
+    },
+    {
+        tipo: "DB_04",
+        name: "SUPABASE/ELASTICSEARCH"
+    },
+    {
+        tipo: "CL_01",
+        name: "AWS/DOCKER"
+    },
+    {
+        tipo: "CL_02",
+        name: "AZURE"
+    },
+    {
+        tipo: "TL_01",
+        name: "KAFKA/GIT/GITHUB"
+    },
+    {
+        tipo: "SO_01",
+        name: "LINUX"
+    },
+    {
+        tipo: "MC_01",
+        name: "C/RASPBERRY"
+    }
+];
